0926be34558477d0f2c7eaeb1b4043b08fe3d9fd
Anton Zakharov <ant.zaharov20177@yandex.ru>
2024-05-05 14:40:59 +0300

    init

---
+++ src/main/java/ru/difembaxio/secutityjwt/SecutityJwtApplication.java
@@ -0,0 +1,13 @@
+package ru.difembaxio.secutityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecutityJwtApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SecutityJwtApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ru/difembaxio/secutityjwt/SecutityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.difembaxio.secutityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecutityJwtApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
